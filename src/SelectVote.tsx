fc5d539660e3954fb7568dd1191e82231ebdf8a5
kametsun <kametsun7@gmail.com>
2024-12-13 10:39:28 +0900

    fix: report index page

--- src/SelectVote.tsx
+++ src/SelectVote.tsx
@@ -1,39 +1,73 @@
-import { Flex, Heading, Link } from "@yamada-ui/react";
+import {
+  Box,
+  Button,
+  CircleProgress,
+  Flex,
+  Heading,
+  Text,
+} from "@yamada-ui/react";
+import { useEffect, useState } from "react";
 
 type GetReportsRes = {
-    reportId: string
-    targetUserName: string
-}
+  reportId: string;
+  userName: string;
+  className: string;
+};
 
-interface Props {
-    reports: GetReportsRes[]
-}
-const SelectVote = (props: Props) => {
-    const sendHandler = () => { }
-    if (props.reports.length == 0) {
-        return (<Flex height="100vh"
-            justify="center"
-            align="center">
-            <Heading as="h2" >欠席報告はありません</Heading>
-        </Flex>)
-    }
+const SelectVote = () => {
+  const url =
+    "https://script.google.com/macros/s/AKfycbxmlNsEeqe9Iw1rDDCkxNrfmmglIjGuoSHCTobuhCUulTCQ7luvr1X5R14o2wPFVWpseg/exec";
+  const [reports, setReports] = useState<GetReportsRes[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchReports = async () => {
+      const body = {
+        api: "getTodayReports",
+      };
+
+      const res = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify(body),
+      });
+      const resText = await res.text();
+      const json = JSON.parse(resText);
+      setReports(json.data.reports);
+      setLoading(false);
+    };
+    fetchReports();
+  }, []);
+
+  if (loading) {
     return (
-        <form onSubmit={sendHandler}>
-            <Flex
-                height="100vh"
-                justify="center"
-                align="center"
-            >
-                <Heading as="h1" size="lg" isTruncated>欠席報告者一覧</Heading><br />
-                <Link href={"/vote"}>
-                    {props.reports.map((report) => (
-                        <button value={report.reportId}>{report.targetUserName}</button>
-                    ))}
+      <Box display="flex" flexDirection="column" alignItems="center" mb={8}>
+        <CircleProgress value={18} isAnimation p={5} />
+        <Text>ローディング中です。</Text>
+      </Box>
+    );
+  }
 
-                </Link>
-            </Flex>
-        </form>
-    )
-}
+  if (reports.length == 0) {
+    return (
+      <Flex height="100vh" justify="center" align="center">
+        <Heading as="h2">欠席報告はありません</Heading>
+      </Flex>
+    );
+  }
+
+  return (
+    <Box>
+      <Heading as="h1" size="lg" isTruncated>
+        欠席報告者一覧
+      </Heading>
+
+      {reports.map((report) => (
+        <Box>
+          <Button>{report.userName}</Button>
+        </Box>
+      ))}
+    </Box>
+  );
+};
 
 export default SelectVote;
