8307bb43a4a25931f53361997544ef8437b09103
kametsun <kametsun7@gmail.com>
2024-12-17 14:27:03 +0000

    add: theme

---
+++ src/.theme/index.ts
@@ -0,0 +1,5 @@
+import { extendTheme, UsageTheme } from "@yamada-ui/react";
+
+const customTheme: UsageTheme = {};
+
+export const theme = extendTheme(customTheme)();
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import { BrowserRouter } from 'react-router-dom'
-import { UIProvider } from '@yamada-ui/react'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import { BrowserRouter } from "react-router-dom";
+import { UIProvider } from "@yamada-ui/react";
+import { theme } from "./.theme";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <UIProvider>
+    <UIProvider theme={theme}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
