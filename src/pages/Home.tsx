ddb2b41de27f1c49bfd799d9a8ac47bd9437a1d8
kametsun <kametsun7@gmail.com>
2024-12-18 12:35:42 +0000

    fix: button commonlize

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,39 +1,19 @@
-import { Button, ButtonGroup, Container, Flex } from "@yamada-ui/react";
+import { Button, Container } from "@yamada-ui/react";
 import { Link } from "react-router-dom";
 
 function Home() {
   return (
-    <Flex height="100vh" justify="center" align="center">
-      <Container>
-        <ButtonGroup
-          gap={"md"}
-          margin="0 auto"
-          display="flex"
-          justifyContent="center"
-        >
-          <Link to={"/contact"}>
-            <Button
-              bg={"Blue.900"}
-              colorScheme={"secondary"}
-              size="2xl"
-              variant={"outline"}
-            >
-              連絡
-            </Button>
-          </Link>
-          <Link to={"/selectVote"}>
-            <Button colorScheme={"secondary"} size="2xl" variant={"outline"}>
-              投票
-            </Button>
-          </Link>
-          <Link to={"/teachers"}>
-            <Button colorScheme={"secondary"} size="2xl" variant={"outline"}>
-              先生
-            </Button>
-          </Link>
-        </ButtonGroup>
-      </Container>
-    </Flex>
+    <Container>
+      <Link to={"/contact"}>
+        <Button>連絡</Button>
+      </Link>
+      <Link to={"/selectVote"}>
+        <Button>投票</Button>
+      </Link>
+      <Link to={"/teachers"}>
+        <Button>先生</Button>
+      </Link>
+    </Container>
   );
 }
 
